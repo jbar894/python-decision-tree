afd62c977877042b10659e0d5ac70138363d4d1f
jbar894 <j.barwellsmith@gmail.com>
2018-05-23 17:08:14 +1200

    set up for using xgboost

--- decision_tree.py
+++ decision_tree.py
@@ -5,73 +5,55 @@ from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeRegressor  
 from sklearn.model_selection import train_test_split  
 from sklearn.externals import joblib
+import xgboost as xgb
 
 passrate = 0.5
 testsize = 0.2
 
+COLUMNS = (
+    'CCFR',
+    'AMFR',
+    'OFR',
+    'SFR',
+    'PFR',
+    'Placed'
+)
+
 #read in data
-dataset = pd.read_csv('./SRData.csv')  
+with open('./SRData.csv', 'r') as train_data:
+    raw_training_data = pd.read_csv(train_data, header=0 ,names=COLUMNS)
 
-X = dataset.drop('Placed', axis=1)  
-y = dataset['Placed'] 
+train_features = raw_training_data.drop('Placed', axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testsize, random_state=0) 
+print(raw_training_data)
 
-regressor = DecisionTreeRegressor()  
-regressor.fit(X_train, y_train) 
+train_labels = (raw_training_data['Placed'] == 1)
 
-joblib.dump(regressor, 'model.joblib')
-
-
-y_pred = regressor.predict(X_test)
-
-df=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})  
-
-#An array used to convert the predicted success into binary
-PS = []
-
-#An array used to store which prections were correct or not
-Correct = []
-n = 0
-
-#A count of correctness
-CC = 0
-
-#Converting to a 0 or 1 with a given pass rate then determining if it is correct or not
-for index, row in df.iterrows():
-    if row[1] >= passrate:
-        PS.append(1)
+for i in range (0,len(train_labels)):
+    if train_labels[i] == True:
+        train_labels[i] = 1
     else:
-        PS.append(0)
-    
-    if row[0] == PS[n]:
-        Correct.append(1)
-        CC+=1
+        train_labels[i] = 0
+
+with open('./SRData.csv', 'r') as test_data:
+    raw_test_data = pd.read_csv(test_data, header=0, names=COLUMNS)
+
+test_features = raw_test_data.drop('Placed', axis=1)
+
+test_labels = (raw_test_data['Placed'] == 1)
+
+for i in range (0,len(test_labels)):
+    if test_labels[i] == True:
+        test_labels[i] = 1
     else:
-        Correct.append(0)
-    n+=1
-
-cdf=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred, 'PS': PS}) 
-print(cdf)
-
-print("Accuracy", CC/n)
-#print("Predicted Score", PS)
-#print("Correct", Correct)
-
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))  
-
-#single job testing
-def singlePredict(job):
-    jobDF = pd.DataFrame(data=job)
-    jobPredicted = sj_pred = regressor.predict(jobDF) 
-    return jobPredicted
+        test_labels[i] = 0
 
 
-sj = {'CCFR':[0.7634], 'AMFR':[0.379310], 'OFR':[0.437500], 'SFR':[0.421875], 'PFR':[0.381119]}
-x = singlePredict(sj)
+print(test_features)
 
-print("Single job prediction", x[0])
+dtrain = xgb.DMatrix(train_features, train_labels)
+dtest = xgb.DMatrix(test_features)
 
+bst = xgb.train({}, dtrain, 20)
+bst.save_model('./model.bst')
 
